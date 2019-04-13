7b81315a9ee4392fc19794540a58fb715839f0ce
Douglas Ray <dpr980@gmail.com>
2019-04-13 14:43:19 +0100

    added typeDefs, resolvers, example mutations and queries, example model, express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('hi')
+import { ApolloServer } from 'apollo-server-express'
+import express from 'express'
+import mongoose from 'mongoose'
+import { resolvers } from './schema/resolvers'
+import { typeDefs } from './schema/typeDefs'
+
+const startServer = async () => {
+	const app = express()
+
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers
+	})
+
+	server.applyMiddleware({ app })
+
+	await mongoose.connect('mongodb://localhost:27017/test3', {
+		useNewUrlParser: true
+	})
+
+	app.listen({ port: 4000 }, () =>
+		console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+	)
+}
+
+startServer()
---
+++ src/schema/resolvers.js
@@ -0,0 +1,23 @@
+import { Cat } from '../models/Cat'
+
+export const resolvers = {
+	Query: {
+		hello: () => 'hi',
+		cats: () => Cat.find()
+	},
+	Mutation: {
+		createCat: async (_, { name }) => {
+			const cat = new Cat({ name })
+			await cat.save()
+			return cat
+		},
+		updateCat: async (_, { id, name }) => {
+			const cat = await Cat.findByIdAndUpdate(id, { name })
+			return cat
+		},
+		deleteCat: async (_, { id }) => {
+			const cat = await Cat.findByIdAndDelete(id)
+			return cat
+		}
+	}
+}
